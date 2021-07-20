3dac5d4f0b9f4e9c2ead40f4850edccc093d5acb
Talia Pichardo <taliapichardo@gmail.com>
2021-07-20 13:16:28 -0400

    Adds seeds to database

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,8 +1,6 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+\c boutique_dev;
+
+INSERT INTO boutique (name, brand, category, price, in_stock, url) VALUES
+('Marmont Bag', 'Gucci', 'handbag', 2200.00, true , 'https://www.pngitem.com/pimgs/m/347-3474254_gucci-marmont-leather-443497-dtdid-1000-hd-png.png'),
+('Supreme Ophidia', 'Gucci', 'handbag', 1490.00, true, 'https://cdn-images.farfetch-contents.com/12/56/25/66/12562566_11923829_480.jpg'),
+('Faux Leather Sweatpants', 'ALICE + OLIVIA', 'clothing', 295.00, true, 'https://static.orchardmile.us/webp/full/alice-and-olivia/bb94a71b314f5556beb1a488bce8cb4dc7b49aa2.webp');
