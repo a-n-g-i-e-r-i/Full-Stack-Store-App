7860059f18079ee5b88bc2544352a42fb1f8cdd1
Talia Pichardo <taliapichardo@gmail.com>
2021-07-20 16:59:44 -0400

    Adds CRUD functionality

--- back-end/controllers/boutiqueController.js
+++ back-end/controllers/boutiqueController.js
@@ -1,11 +1,68 @@
 const express = require("express");
-const { getAllItems } = require("../queries/stock");
+const { getAllItems, getItem, addItem, updateItem, deleteItem } = require("../queries/stock");
 
 const boutique = express.Router();
 
 boutique.get("/", async (req, res) => {
     const allItems = await getAllItems();
     res.json(allItems);
+});
+
+boutique.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const item = await getItem(id);
+        if (item.id) {
+            res.json(item)
+        } else {
+            throw `Item not found`;
+        }
+    } catch (e) {
+        res.status(404).json({ error: "NOT BOUJEE ENOUGH", message: e });
+    }
+});
+
+boutique.post("/", async (req, res) => {
+    try {
+        const item = await addItem(req.body);
+        if (item["id"]) {
+            res.json(item);
+        } else {
+            console.log(`Database error: ${item}`);
+            throw `Error adding ${req.body} to database`;
+        }
+    } catch (e) {
+        res.status(404).json({ error: "NOT BOUJEE ENOUGH TO ADD.", message: e });
+    }
+});
+
+boutique.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleted = await deleteItem(id)
+        if (deleted.id) {
+            res.json(deleted);
+        } else {
+            throw "Resource not found.";
+        }
+    } catch (error) {
+        res.status(404).json({ error: "CANNOT DELETE BOUJEE", message: error })
+    }
+});
+
+
+boutique.put("/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const item = await updateItem(id, req.body)
+        if (item.id) {
+            res.json(item)
+        } else {
+            throw `There is no item with id: ${id}`
+        }
+    } catch (e) {
+        return e
+    }
 });
 
 module.exports = boutique;
--- back-end/queries/stock.js
+++ back-end/queries/stock.js
@@ -9,8 +9,52 @@ const getAllItems = async () => {
     }
 };
 
+const getItem = async (id) => {
+    try {
+        const item = await db.one("SELECT * FROM boutique WHERE id=$1", id);
+        return item;
+    } catch (err) {
+        return err;
+    }
+};
 
+const addItem = async (item) => {
+    try {
+        if (!item.name) {
+            throw 'You must specify a value for name';
+        }
+        const newItem = await db.one("INSERT INTO boutique (name, brand, category, price, in_stock, url) VALUES($1, $2, $3, $4, $5, $6) RETURNING *", [item.name, item.brand, item.category, item.price, item.in_stock, item.url]);
+        return newItem;
+    } catch (error) {
+        return error
+    }
+}
+
+const updateItem = async (id, item) => {
+    try {
+        return await db.one(
+            "UPDATE boutique SET name=$1, brand=$2, category=$3, price=$4, in_stock=$5, url=$6  WHERE id=$7 RETURNING *",
+            [item.name, item.brand, item.category, item.price, item.in_stock, item.url, id]
+        );
+    } catch (e) {
+        return e;
+    }
+};
+
+const deleteItem = async (id) => {
+    try {
+        const deleted = await db.one("DELETE FROM boutique WHERE id=$1 RETURNING *", id)
+        return deleted;
+    } catch (error) {
+        return error
+    }
+
+}
 
 module.exports = {
     getAllItems,
+    getItem,
+    addItem,
+    updateItem,
+    deleteItem,
 }
