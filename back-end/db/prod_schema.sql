e7d6d06df2b097a3b37a29ee1914f631fb79622c
Tristan Angieri <tristanangieri@Tristans-MacBook-Pro.local>
2021-07-21 12:48:19 -0400

    deploy with prod_dbschema and seed

---
+++ back-end/db/prod_schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS d9k45j79g8bh4v;
+CREATE DATABASE d9k45j79g8bh4v;
+
+\c d9k45j79g8bh4v;
+
+CREATE TABLE boutique (
+    id SERIAL PRIMARY KEY, 
+    name TEXT,
+    brand TEXT,
+    category TEXT,
+    price INT,
+    in_stock BOOLEAN,
+    url TEXT
+);
---
+++ back-end/db/prod_seed.sql
@@ -0,0 +1,10 @@
+\c d9k45j79g8bh4v;
+
+INSERT INTO boutique (name, brand, category, price, in_stock, url) VALUES
+('Marmont Bag', 'Gucci', 'handbag', 2200.00, true , 'https://www.pngitem.com/pimgs/m/347-3474254_gucci-marmont-leather-443497-dtdid-1000-hd-png.png'),
+('Supreme Ophidia', 'Gucci', 'handbag', 1490.00, true, 'https://cdn-images.farfetch-contents.com/12/56/25/66/12562566_11923829_480.jpg'),
+('Faux Leather Sweatpants', 'ALICE + OLIVIA', 'clothing', 295.00, true, 'https://static.orchardmile.us/webp/full/alice-and-olivia/bb94a71b314f5556beb1a488bce8cb4dc7b49aa2.webp'),
+('Ski 5 Wrap-Around Sunglassess', 'Christian Dior', 'sun glassess', 75.00, false, 'https://i.pinimg.com/originals/4e/1f/d3/4e1fd391305a1f3011c439f001af56c9.jpg' ),
+('Gold Plated Sunglasses AW 2000', 'Dior', 'sun glassess', 90.00, false, 'https://dtpmhvbsmffsz.cloudfront.net/posts/2017/07/03/595a0554291a35dd960a875c/m_595a055b78b31c74200a6d49.jpg'),
+('Hermes Clic', 'Hermes', 'handbag', 7550.00, true, 'https://assets.hermes.com/is/image/hermesproduct/hermes-clic-h-33-bag--077857CK89-front-1-300-0-850-850_b.jpg'),
+('Takashi Murakami', 'Louis Vuitton', 'handbag', 1395.00, false, 'https://cdn.shopify.com/s/files/1/1248/8217/products/803e3cd3c1410f61ee939257a7c88173_2000x.jpg?v=1605197755');
